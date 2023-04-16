4078ee5c34fd75449d4997edad426222da9a3073
Ken Kousen <ken.kousen@kousenit.com>
2023-04-16 11:09:20 -0400

    Change ControllerAdvice to use superclass and return ProblemDetail

--- reactive-customers/src/main/java/com/kousenit/reactivecustomers/controllers/CustomerAdvice.java
+++ reactive-customers/src/main/java/com/kousenit/reactivecustomers/controllers/CustomerAdvice.java
@@ -1,16 +1,18 @@
 package com.kousenit.reactivecustomers.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class CustomerAdvice {
+public class CustomerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleIllegalArgumentException(IllegalArgumentException e) {
-        return e.getMessage();
+    public ProblemDetail handleIllegalArgumentException(IllegalArgumentException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
     }
 }
